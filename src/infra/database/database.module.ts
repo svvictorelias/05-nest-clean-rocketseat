b2db799c5fda2694eddb632db99abbe47f602515
Victor Elias <svvictorelias@gmail.com>
2024-02-10 21:20:18 -0300

    first commit

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma/prisma.service'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+
+@Module({
+  providers: [
+    PrismaService,
+    { provide: QuestionsRepository, useClass: PrismaQuestionsRepository },
+    PrismaQuestionAttachmentsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswersRepository,
+  ],
+  exports: [
+    PrismaService,
+    QuestionsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswersRepository,
+  ],
+})
+export class DatabaseModule {}
